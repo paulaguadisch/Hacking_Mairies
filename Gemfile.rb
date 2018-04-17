7365dc41aff06f024b3ceece86f8497de2f1513a
Paul A <paul.aguadisch@gmail.com>
2018-04-17 17:57:55 +0200

    ajout du fichier Gemfile à modifier

---
+++ Gemfile.rb
@@ -0,0 +1,11 @@
+source "https://rubygems.org"
+
+gem "ruby", ">= 2.3.4p301"
+gem "nokogiri", ">= 2.3.4"
+require 'rubygems'
+gem "nokogiri", ">= 1.8.2"
+require 'open-uri'
+require 'google_drive'
+require 'gmail'
+require 'csv' 
+require 'dotenv'
